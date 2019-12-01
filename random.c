801ec792d67f4b5b187e0d5b45148418971ae051
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2019-12-01 21:26:52 +0000

    Create random.c

---
+++ random.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+ 
+int main(void)
+{
+    int i;
+ 
+    srand(time(NULL));
+    i = 1000+(rand()%9000);
+ 
+    puts("Pin aleatório:");
+    printf("%d\n",a);
+}
