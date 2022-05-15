12ded892292f9100274b8ad1e4ca451e48720242
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-15 22:51:03 +0200

    Update matematica.c

--- matematica.c
+++ matematica.c
@@ -1,27 +1,27 @@
 #include <stdio.h> //biblioteca padrão para comandos i/o
-int main (void)
+float main (void)
 {
-  int valor1, valor2;
-  int resp_soma, resp_subtracao, resp_multiplicacao, resp_divisao;
+  float valor1, valor2;
+  float resp_soma, resp_subtracao, resp_multiplicacao, resp_divisao;
  
   printf("Testando operadores aritmeticos\n\n");
   // \n desloca cursor para a próxima linha
  
   printf("Digite o primeiro valor: ");
-  scanf("%d", &valor1);
+  scanf("%f", &valor1);
   printf("Digite outro valor: ");
-  scanf("%d", &valor2);
+  scanf("%f", &valor2);
  
   resp_soma = valor1 + valor2;
-  printf("\nSoma = %d", resp_soma);
+  printf("\nSoma = %g", resp_soma);
  
   resp_subtracao = valor1 - valor2;
-  printf("\nSubtracao = %d", resp_subtracao);
+  printf("\nSubtracao = %g", resp_subtracao);
  
   resp_multiplicacao = valor1 * valor2;
-  printf("\nMultiplicacao = %d", resp_multiplicacao);
+  printf("\nMultiplicacao = %g", resp_multiplicacao);
  
   resp_divisao = valor1 / valor2;
-  printf("\nDivisao = %d\n", resp_divisao);
+  printf("\nDivisao = %g\n", resp_divisao);
   return (0); //valor de retorno da função main
 }
