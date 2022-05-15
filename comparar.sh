093c1bb19ff72ada2e0c999bfe06572bb3905125
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-15 18:31:33 +0200

    Update comparar.sh

--- comparar.sh
+++ comparar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect -f
-set timeout 5
+set timeout 1
 spawn ./comparar
 expect "insere o primeiro valor:\r"
 send -- "5\r"
 expect "Insere o segundo valor:\r"
-send -- "7"
+send -- "7\r"
 expect eof
