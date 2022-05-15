f5251af6038d8610c55297d24017dd1925e0011a
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-15 11:59:46 +0200

    Create ciclo.c

---
+++ ciclo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int num=1;     // inicializando a variável
+    while(num<=10) // enquanto ciclo com condição
+    {
+        printf("%d\n",num);
+        num++;     // operação de incremento
+    }
+    return 0;
+}
