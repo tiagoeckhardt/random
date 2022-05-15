837b7f54a257fdd19ec4a2f54e1f3b19c6084616
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-15 12:39:43 +0200

    Create comparar.c

---
+++ comparar.c
@@ -0,0 +1,27 @@
+//programa exemplo de comparação
+#include <stdio.h>
+#define se if
+#define principal main
+#define imprima printf
+#define leia scanf
+#define devolva return
+#define por puts
+
+int principal()
+{
+    int primeiro,segundo;
+    imprima(insere o primeiro valor:");
+    leia("%d",&primeiro);
+    imprima("Insere o segundo valor:");
+    leia("%d",&segundo);
+    por ("A determinar o menor e maior valor..\n");
+    se(primeiro<segundo)
+    {
+        imprima("%d é menor que %d.\n",primeiro,segundo);
+    }
+    se(primeiro>segundo)
+    {
+        imprima("%d é maior que %d.\n",primeiro,segundo);
+    }
+    devolva(0);
+}
