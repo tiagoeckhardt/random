f9418353459ab451a650630ba4983e53be52029f
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-15 23:36:59 +0200

    Create petalas.c

---
+++ petalas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+int num;
+printf("Digite o número de pétalas: ");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("Mal me quer.");
+}
+else
+printf("Bem me quer.");
+}
