d53d7230948f805824eb1b383aa850054afd9fd2
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-20 19:59:07 +0200

    Create decisao.c

---
+++ decisao.c
@@ -0,0 +1,14 @@
+\\ Programa exemplo
+#include <stdio.h>
+
+int main()
+{
+int a;
+printf("Escreva um número de zero a cem:");
+scanf("%d", &a);
+if (a >= 0 && a <= 100){
+  printf ("Sucesso!");
+  }
+  else
+          printf ("Ups!");
+}
