4d038b43ed177bfc95c23850fa0fcc9d3f297ec3
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-20 21:31:25 +0200

    Update floresta.sh

--- floresta.sh
+++ floresta.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/expect -f
 set timeout 1
-spawn ./floresta
-expect "Qual a maior floresta do mundo:\r"
-send -- "1\r"
+spawn ./nota
+expect "Insira a primeira nota: \r"
+send -- "15\r"
+expect "Insira a segunda nota: \r"
+send -- "20\r"
 expect eof
