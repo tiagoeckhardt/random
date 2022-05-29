b7f47940d2647064a5e08b200f8a7302998118b6
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-29 20:44:55 +0200

    Create programar.c

---
+++ programar.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+  char frase[] ="Vamos programar?";
+  char * palavra;
+  palavra = strstr (frase,"programar");
+  strncpy (palavra,"estudar.",9);
+  puts (frase);
+  return 0;
+}
