07ea9aac438f0ea00c8e1239544628d0bece7b2c
tiagoeckhardt <53041802+tiagoeckhardt@users.noreply.github.com>
2022-05-29 21:14:47 +0200

    Create local.c

---
+++ local.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int a = 1; // Variável global
+void main()
+{    
+int b = 2; // Variável local    
+
+printf("Valor da variável 'a': %d", a);    
+printf("\nValor da variável 'b': %d", b);
+}
